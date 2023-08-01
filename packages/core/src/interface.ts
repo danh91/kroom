de974a444cbb9f81fb5e9b75880efce93b601603
Daniel K <danielk.developer@gmail.com>
2023-08-01 15:23:34 -0700

    (wip) design no-code interfaces

--- packages/core/src/interface.ts
+++ packages/core/src/interface.ts
@@ -1,3 +1,59 @@
+interface Trigger {
+  id: string;
+  name: string;
+  description: string;
+  fields: Field[];
+}
+
+interface Action {
+  id: string;
+  name: string;
+  description: string;
+  fields: Field[];
+  execute: (fields: Record<string, any>) => Promise<void>;
+}
+
+interface Field {
+  id: string;
+  name: string;
+  type: FieldType;
+  required: boolean;
+  options?: string[];
+}
+
+enum FieldType {
+  Text = "text",
+  Number = "number",
+  Boolean = "boolean",
+  Select = "select",
+  MultiSelect = "multi-select",
+  Date = "date",
+  Time = "time",
+  DateTime = "datetime",
+}
+
+interface Workflow {
+  id: string;
+  name: string;
+  description: string;
+  triggers: Trigger[];
+  actions: Action[];
+  connections: Connection[];
+}
+
+interface Connection {
+  id: string;
+  triggerId: string;
+  actionId: string;
+  mappings: Mapping[];
+}
+
+interface Mapping {
+  sourceFieldId: string;
+  targetFieldId: string;
+}
+
+
 export const add = (a: number, b: number) => {
     return a + b;
 };
